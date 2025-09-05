2442c5f0584887d7f2a220d881742694e376e2ab
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2025-09-05 13:06:10 +0200

    feat: update project colors and add deploy workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/denis_visma',
+  assetPrefix: '/denis_visma/',
   eslint: {
     ignoreDuringBuilds: true,
   },
