09670807b59afdd9574fa9309a7ea0b643da4c66
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2025-09-05 13:21:33 +0200

    chore: switch to vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/denis_visma',
-  assetPrefix: '/denis_visma/',
   eslint: {
     ignoreDuringBuilds: true,
   },
